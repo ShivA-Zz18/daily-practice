7f323dfdbb4153d9f5ac3abb4e1b2812346cd964
SHIVA-ADIGA-18 <shiva959154@gmail.com>
2024-10-15 14:59:02 +0530

    Add files via upload

---
+++ day16pt.js
@@ -0,0 +1,131 @@
+// let pass="  hello  "; 
+// console.log(pass.trim());
+
+
+// let pass=prompt("Enter the password");
+// console.log(pass.trim());
+
+
+// let name="shiva";
+// console.log(name.toUpperCase());
+
+   
+// let msg="  hello  ";
+// let newmsg=msg.trim().toUpperCase();
+// console.log(newmsg);
+
+
+// let str= "shiva";  
+// console.log(str.indexOf("a","s"));
+
+
+// let msg="   hello   ";
+// console.log(msg.toUpperCase().trim());
+
+
+// let msg="shivakumar";
+// console.log(msg.slice(5,10));   
+
+// let str="i LOVE coading";
+// console.log(str.repeat(3));
+
+// let msg =" help! ";
+// console.log(msg.trim().toUpperCase());
+
+
+// let msg="apnacollege";
+// console.log(msg.indexOf("na"));
+
+
+// let msg1="apn badsha";
+// console.log(msg1.replace("apn","sudip"));
+
+// let word="nimma ajji";
+// console.log(word.replace("j","m").replace("j","m").toUpperCase());
+
+
+//   let req=prompt("enter an value")
+//   if
+//     (req=="namasthe"){
+//         console.log("ha namasthe mathe en samachara ");
+//     }
+
+//     else if(req=="sanjay "){
+//         console.log("avna gandu nan maga");
+//     }
+//     else if(req=="vinay"){
+//         console.log("avna gay nan maga");
+//     }
+
+    
+
+//     else{
+//         console.log("en type madtha ediyo sari type maadu")
+//     }
+  
+//   let name=["shraddha","aman","raju"];
+//   console.log(name);
+
+
+// let mixedarrey=["shraddha",69,12.5];
+// console.log(mixedarrey);
+
+
+//   let cars=["audi","bmw","murzidiz","toyota"];
+//   console.log(cars.push("ambasidor"));
+
+
+//   let cars=["audi","bmw","murzidiz","toyota","ambasidor"];
+//   console.log(cars.pop());
+
+
+// let cars=["audi","bmw","murzidiz","toyota"];
+// console.log(cars.unshift("ambasidor"));
+
+// let cars=["audi","bmw","murzidiz","toyota"];
+// console.log(cars.shift());
+ 
+
+//   let months=["jan","feb","mar","april"];
+//   console.log(months.shift());  
+//   console.log(months.shift());  
+
+// console.log(months.unshift("june","july"));
+// console.log(months)
+  
+
+// let cars=["audi","bmw","mercidize"];
+// console.log(cars.indexOf("bmw")); 
+
+
+// let cars=["audi","bmw","mercidize"];
+// console.log(cars.includes("bmw")); 
+
+
+// let color=["red ","green","yello"];
+// let actors=["sharu","amithab","salman"];
+// console.log(color.concat(actors));
+
+// let months=["jan","feb","march","april"];
+// console.log(months.reverse());
+
+
+// let janwar=["manya","adithi","sevanth","gopi","sam"];
+// console.log(janwar.slice());
+// console.log(janwar.slice(2,3));
+
+// let boys=["darshan","sudeep","dananjay","ambareesh","vishnu"];
+// console.log(boys.splice(1,1,"shankar")); 
+
+
+// let boys=["darshan","sudeep","dananjay","ambareesh","vishnu"];
+// console.log(boys.sort());  
+
+let months=["jan","feb","march","april"];
+console.log(months.splice(0,1,"dec"));
+console.log(months);
+
+
+
+
+
