16fc2fe35dcf5606b6079b963cf795918d0fb83b
SHIVA-ADIGA-18 <shiva959154@gmail.com>
2024-10-17 22:35:18 +0530

    Add files via upload

---
+++ day17pt.js
@@ -0,0 +1,110 @@
+// console.log("hello");
+  
+// let i=prompt("Enter a valuve")
+
+//  for(let i=1;i<=100;i++){
+//     console.log(i);
+
+//  }
+
+
+// for(let i=10;i>=1;i--){
+//     console.log(i); 
+// }
+
+
+//  for(i=1;i<=30;i=i+2){
+//     console.log(i);
+//  }
+
+// console.log("backwards");
+
+// for(i=15;i>=1;i=i-2){
+//     console.log(i)
+// }
+
+//   for(i=2;i<=10;i=i+2){
+//     console.log(i);
+//   }
+
+//   console.log("backewards");
+
+//   for(let i=20;i>=1;i=i-2)
+// {
+//     console.log(i)
+// } 
+
+// console.log("helo");
+
+// for(let i=5;i<=50;i=i+5){
+//   console.log(i);
+// }
+
+// let n=prompt("Enter a number");
+// n=parseInt(n);
+// for(i=n;i<=n*10;i=i+n){
+//   console.log(i);
+// }
+
+// for(i=1;i<=3;i++){
+//   console.log(`outer loop${i}`);
+//   for(j=1;j<=3;j++){
+//     console.log(j);
+//   }
+
+// } 
+
+// let i=1;
+// while(i<=50){
+//   console.log(i);
+//   i++;
+// } 
+
+// console.log("backward");
+// let i=50;
+// while(i>=1){
+//   console.log(i);
+//   i--;
+// }
+ 
+//  const favmovie="avathar";
+  
+//  let guess=prompt("Enter the movie name");
+
+//  while((guess!=favmovie)){
+// if(guess=="quit"){
+//    console.log("you quit the game ");
+//    break;
+// }
+
+//     guess=prompt("plese try again");  
+//  }
+
+//  if(guess==favmovie){
+//    console.log("congrats you entered a write answer");
+//  }
+
+ 
+// let fruits=["appale","banaana","litchi","mango","pinaple","kiwi"];
+
+// for(i=0;i<fruits.length;i++){
+//    console.log(i,fruits[i]);
+// }
+  
+ 
+// let fruits=["appale","banaana","litchi","mango","pinaple","kiwi"];
+// for(i=fruits.length-1;i>=0;i--){
+//    console.log(i,fruits[i]);
+// }
+
+
+let heroes=[["iron man","super man","spider man","thor"],
+["wonder women","splash","jakichan"]];
+
+ for(i=0;i<heroes.length;i++){
+   console.log(i,heroes[i],heroes[i].length);
+
+   for(let j=0;j<heroes[i].length;j++){
+      console.log(`j=${j},${heroes[i],[j]}`) ;
+   }
+ } 
