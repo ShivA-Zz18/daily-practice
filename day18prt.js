2dcb84a074491f2e188f3ecc755615844ee3e131
SHIVA-ADIGA-18 <shiva959154@gmail.com>
2024-10-21 22:38:27 +0530

    Add files via upload

---
+++ day18prt.js
@@ -0,0 +1,147 @@
+// console.log("helo");
+
+
+//  let student={
+//     name:"shiva",
+//     class:"bca",
+//     rollno:"ca23528",
+//     marks:100
+//  };
+//  console.log(student);
+
+
+//   let items={
+//     names : ["cakkuli","sonpapdi","halwa","payasa"],
+//     price:100,
+//     discount:70,
+//     color:["red","green","yellow","brown"]
+
+//   };
+
+//   console.log(items);
+  
+
+
+//  let post={
+//     username:"shiva",
+//     content:"coding related information and web dev resourses",
+//     likes:100,
+//     repost:10,
+//     tags:["@bcafamily","@bcasquad","@bcahub"]
+//  };
+
+//  console.log(post);
+// console.log( post["content"]);
+
+// console.log(post.likes);
+
+// let prop="repost";
+// console.log(post[prop]);
+
+
+// obj={
+//     1:"a",
+//     2:"b",
+//     null:"c",
+//     undefined:"d",
+//     error:"e"
+// };
+
+// console.log(obj["error"]);
+// console.log(obj["undefined"]);
+
+
+ 
+
+//   let student={
+//     name:"shiva",
+//     age:18,
+//     marks:80.6,
+//     city:"udupi"
+//   };
+
+//  changing and updating the literral values//
+
+//  console.log( student["name"]="manya",student["city"]="mumbai");
+//  console.log(student["gender"]="female",student["age"]=19,student["marks"]="A");
+//  console.log(delete student["city"]);
+//  console.log(student);
+
+
+//  const studentinfo={
+
+// shiva:{
+// grade:"a",
+// city:"karkala"
+// },
+
+// manya:{
+//     grade:"a+",
+//     city:"udupi"
+
+// },
+
+// neeksha:{
+// grade:"o",
+// city:"manglore"
+// }
+
+//  };
+
+//  console.log(studentinfo.manya.grade);
+//  console.log(studentinfo.neeksha.city="dubai");
+//  console.log(studentinfo);
+
+
+
+
+ 
+//   let classinfo=[
+// {
+//     name:"shiva",
+//     course:"bca",
+//     city:"karkala"
+// },
+
+// {
+//     name:"manya",
+//     course:"B.com",
+//     city:"udupi"
+// },
+
+// {
+//     name:"adithi",
+//     course:"BBA",
+//     city:"moodubidre"
+// }
+//   ];
+// console.log(classinfo[1].name);
+// console.log(classinfo[0].name="ashik");
+//   console.log(classinfo);
+
+
+
+
+console.log(Math.abs(-10.3));
+
+// console.log(Math.pow(10,3));
+
+
+// console.log(Math.floor(10.6));
+
+// console.log(Math.ceil(10.33));
+
+// console.log(Math.random());
+
+
+
+
+
+
+
+  
+
+
+
+ 
+  
