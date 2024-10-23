2fb36e63aed7e81864263001ecb0f994221b5bca
Shiva kumar <shiva959154@gmail.com>
2024-10-23 06:24:23 +0530

    Add files via upload

---
+++ day19.js
@@ -0,0 +1,129 @@
+// console.log("hello");
+  
+
+// function greet(){
+//     console.log("Have a good day !");
+    
+// }
+
+// greet();
+
+
+// function loop1to5(){
+//     for(i=1;i<=5;i++){
+//         console.log(i);
+        
+//     }
+// }
+
+// loop1to5();
+// loop1to5();
+// loop1to5();
+
+
+
+// function isAdult(){
+//     let age=19;
+//     if(age>=18){
+//         console.log("adult");
+
+//      } else{
+//             console.log("not an adult child chapathi");
+            
+//         }
+        
+//     }
+
+
+//     isAdult();
+
+
+
+// function printpoem(){
+//     console.log("Twinkel Twinkel littel star");
+//     console.log("How i wonder what you i");
+//     console.log("up above the world so high");
+//     console.log("likel dimend in the sky");
+// }
+
+// printpoem();
+
+
+// function dise(){
+//     console.log(Math.floor(Math.random()*6)+1 );
+    
+// }
+
+// dise();
+
+
+
+//  function stuinfo(name,age,city){
+// console.log(`${name}'s age is ${age} and city is ${city}`);
+
+//  }
+
+//  stuinfo("shiva",18,"udupi");
+//  stuinfo("manya",18,"karkala");
+//  stuinfo("sevanth",18,"sulia");
+//  stuinfo("adithi",18,"malpe");
+
+
+
+// function sum(a,b){
+// console.log(a+b);
+
+// }
+
+// sum(1,2);
+// sum(4,6);
+// sum(7,2);
+// sum(11,21);
+
+
+//  function avgcalc(a,b,c){
+//     let avg=(a+b+c)/3;
+//     console.log(avg);
+    
+//  }
+  
+//  avgcalc(45,100,170);
+//  avgcalc(20,40,55);
+//  avgcalc(11,34,67);
+//  avgcalc(56,21,89);
+
+
+// function pritable(n){
+    
+//     for(let i=n;i<=n*10;i+=n){
+//         console.log(i);  
+//     }
+// }
+
+// pritable(10);
+
+
+
+// function getsum(n){
+//     let sum=0;
+//     for(let i=1;i<=n;i++){
+//         sum=sum+i;
+//     }
+//     return sum;
+// }
+
+
+
+ let str=["my","name","is","kaat","manya"];
+
+ function concat(str){ 
+    let result="";
+
+    for(let i=0;i<str.length;i++){
+        result += str[i];
+    }
+    return result;
+ }
+
+ console.log(concat(str));
+ 
