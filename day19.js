4fda59dbeb4518f5d60264e8bf0907176f6d2fcb
Shiva kumar <shiva959154@gmail.com>
2024-10-24 23:01:01 +0530

    Add files via upload

--- day19.js
+++ day19.js
@@ -1,129 +1,225 @@
-console.log("hello");
+// console.log("hello");
   
 
-function greet(){
-    console.log("Have a good day !");
+// function greet(){
+//     console.log("Have a good day !");
     
-}
+// }
 
-greet();
+// greet();
 
 
-function loop1to5(){
-    for(i=1;i<=5;i++){
-        console.log(i);
+// function loop1to5(){
+//     for(i=1;i<=5;i++){
+//         console.log(i);
         
-    }
-}
+//     }
+// }
 
-loop1to5();
-loop1to5();
-loop1to5();
+// loop1to5();
+// loop1to5();
+// loop1to5();
 
 
 
-function isAdult(){
-    let age=19;
-    if(age>=18){
-        console.log("adult");
+// function isAdult(){
+//     let age=19;
+//     if(age>=18){
+//         console.log("adult");
 
-     } else{
-            console.log("not an adult child chapathi");
+//      } else{
+//             console.log("not an adult child chapathi");
             
-        }
+//         }
         
-    }
+//     }
 
 
-    isAdult();
+//     isAdult();
 
 
 
-function printpoem(){
-    console.log("Twinkel Twinkel littel star");
-    console.log("How i wonder what you i");
-    console.log("up above the world so high");
-    console.log("likel dimend in the sky");
-}
+// function printpoem(){
+//     console.log("Twinkel Twinkel littel star");
+//     console.log("How i wonder what you i");
+//     console.log("up above the world so high");
+//     console.log("likel dimend in the sky");
+// }
 
-printpoem();
+// printpoem();
 
 
-function dise(){
-    console.log(Math.floor(Math.random()*6)+1 );
+// function dise(){
+//     console.log(Math.floor(Math.random()*6)+1 );
     
-}
+// }
 
-dise();
-
-
-
- function stuinfo(name,age,city){
-console.log(`${name}'s age is ${age} and city is ${city}`);
-
- }
-
- stuinfo("shiva",18,"udupi");
- stuinfo("manya",18,"karkala");
- stuinfo("sevanth",18,"sulia");
- stuinfo("adithi",18,"malpe");
+// dise();
 
 
 
-function sum(a,b){
-console.log(a+b);
+//  function stuinfo(name,age,city){
+// console.log(`${name}'s age is ${age} and city is ${city}`);
 
-}
+//  }
 
-sum(1,2);
-sum(4,6);
-sum(7,2);
-sum(11,21);
+//  stuinfo("shiva",18,"udupi");
+//  stuinfo("manya",18,"karkala");
+//  stuinfo("sevanth",18,"sulia");
+//  stuinfo("adithi",18,"malpe");
 
 
- function avgcalc(a,b,c){
-    let avg=(a+b+c)/3;
-    console.log(avg);
+
+// function sum(a,b){
+// console.log(a+b);
+
+// }
+
+// sum(1,2);
+// sum(4,6);
+// sum(7,2);
+// sum(11,21);
+
+
+//  function avgcalc(a,b,c){
+//     let avg=(a+b+c)/3;
+//     console.log(avg);
     
- }
+//  }
   
- avgcalc(45,100,170);
- avgcalc(20,40,55);
- avgcalc(11,34,67);
- avgcalc(56,21,89);
+//  avgcalc(45,100,170);
+//  avgcalc(20,40,55);
+//  avgcalc(11,34,67);
+//  avgcalc(56,21,89);
 
 
-function pritable(n){
+// function pritable(n){
     
-    for(let i=n;i<=n*10;i+=n){
-        console.log(i);  
-    }
-}
+//     for(let i=n;i<=n*10;i+=n){
+//         console.log(i);  
+//     }
+// }
 
-pritable(10);
-
-
-
-function getsum(n){
-    let sum=0;
-    for(let i=1;i<=n;i++){
-        sum=sum+i;
-    }
-    return sum;
-}
+// pritable(10);
 
 
 
- let str=["my","name","is","kaat","manya"];
+// function getsum(n){
+//     let sum=0;
+//     for(let i=1;i<=n;i++){
+//         sum=sum+i;
+//     }
+//     return sum;
+// }
 
- function concat(str){ 
-    let result="";
 
-    for(let i=0;i<str.length;i++){
-        result += str[i];
-    }
-    return result;
- }
 
- console.log(concat(str));
+//  let str=["my","name","is","kaat","manya"];
+
+//  function concat(str){ 
+//     let result="";
+
+//     for(let i=0;i<str.length;i++){
+//         result += str[i];
+//     }
+//     return result;
+//  }
+
+//  console.log(concat(str));
+
+
+// let sum=45;
+
+// function calc(a,b){
+// let sum =a+b;  
+// console.log(sum);
+
+// } 
+
+// calc(5,6);
+// console.log(sum);
+
+
+//  let age=20;
+//  if(age>18){
+//    let  str="adult";
+//     console.log(str);
+    
+//  }
+
+
+
+
+// function outer(){
+//     let a =10;
+//     let b=29;
+    
+//     function inner(){
+      
+//         console.log(a);
+//         console.log(b);
+        
+        
+//     }
+//     inner();
+    
+// }
+
+// console.log(outer());
+
+
  
+
+//  let greet="hello"; //global scope
+
+//  function changegreet(){ //function scope
+//     let greet="namasthe";
+//     console.log(greet);
+
+//     function innerchangegree(){ //lexical scope 
+//         console.log(greet);
+        
+//     }
+//     innerchangegree();
+//  }
+
+//  console.log(greet);
+//  changegreet();
+
+
+
+
+
+//  let sum=function(a,b){
+//     return a+b;
+
+//  }
+// console.log( sum(10,18));
+
+
+
+// let hello=function(){
+//     console.log("GOOD EVENING");
+    
+// }
+
+// hello();
+
+
+let hello=function(){
+    console.log("GOOD NIGHT");
+    
+}
+ 
+hello();
+
+
+
+
+
+
+
+
+
+ 
+
