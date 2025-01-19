519f55e07d1d34847bab20a62f2d10018e773471
Shiva kumar <shiva959154@gmail.com>
2025-01-19 09:44:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+// console.log("Hello");
+
+
+
+// function Arry(){
+//     let Array=["Sman","rajath","Vikyath","Varun"];
+//     console.log(Array);
+    
+// }
+
+// Arry();
+
+
+
+// function poem(){
+// console.log("jony jony YEss papap");
+// console.log("Eating shugar no papap");
+// console.log("Up above the hands of high");
+// console.log("like dimend in the sky");
+
+
+// }
+
+// poem();
+
+
+
+
+// function diseprint() {
+
+//     let dise=Math.floor(Math.random()*6)+1
+
+//     console.log(`Dise number is : ${dise}`);
+    
+    
+// }
+
+// diseprint();
+
+
+// function stdInfo(name,age,gender){
+//     console.log(`student name is:${name} & age is ${age} gennder is ${gender}`);
+    
+// }
+
+// stdInfo("rajath",18,"Male");
+
+
+// function printavg(a,b,c){
+//      avg =a+b+c/3;
+//      console.log(avg);
+     
+// }
+
+// printavg(10,20,30);
+
+
+function table(n){
+   
+for(let i=n;i<=n*10;i=i+n){
+    console.log(i);
+    
+}
+
+}
+
+table(10);
