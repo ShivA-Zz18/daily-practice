701915137d312df8e53d849131738af276bc0326
Shiva kumar <shiva959154@gmail.com>
2025-02-10 21:23:37 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -37,10 +37,53 @@ console.log("HEllo");
 // document.querySelector("body").append(div);
 
 
-let button = document.createElement("button");
+// let button = document.createElement("button");
 
-button.innerText=("click me")
+// button.innerText=("click me")
 
-document.querySelector("body").append(button);
+// document.querySelector("body").append(button);
+
+
+// let button = document.querySelector("button");
+
+// button.setAttribute("button","btn");
+
+
+
+let btn =  document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    let h3= document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText= randomColor;
+
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor= randomColor;
+
+    console.log("Color Updated");
+    
+
+});
+
+
+
+function getRandomColor(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+
+
+    let Color = `rgb(${red},${green},${blue})`;
+    return Color;
+}
+
+  
+
+
+
+
+
+
 
 
