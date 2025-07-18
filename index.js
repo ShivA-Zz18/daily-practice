93a72f84a6bd705e020137b3828d23f2fba29437
Shiva kumar <shiva959154@gmail.com>
2025-07-18 19:05:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port =  3000;
+const port = 8080;
+const path = require("path");
 
-app.get("/register", (req, res) => {
-  res.send('Hello World!');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({ extended: true }));
+
+let posts = [
+    {
+        id: 1,
+        username: "shiva",
+        content: "i love watching movie"
+    },
+    {
+        id: 2,
+        username: "aman",
+        content: "i love watching coding"
+    },
+    {
+        id: 3,
+        username: "samnth",
+        content: "my goal is to get a job in a good company"
+    }
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts: posts });
 });
-
-
-app.post("/register", (req, res) => {
-  res.send('Hello World!');
-});
-
-
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-}); 
+    console.log("Listening to port:", port);
+});
