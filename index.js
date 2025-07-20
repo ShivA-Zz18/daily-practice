63f8bf0e8c4172140e4a53fa7ef59124ed522c16
Shiva kumar <shiva959154@gmail.com>
2025-07-20 21:45:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
 const port = 8080;
@@ -12,25 +13,45 @@ app.use(express.urlencoded({ extended: true }));
 
 let posts = [
     {
-        id: 1,
+        id: "1a",
         username: "shiva",
         content: "i love watching movie"
     },
     {
-        id: 2,
+        id: "2b",
         username: "aman",
         content: "i love watching coding"
     },
     {
-        id: 3,
+        id: "3c",
         username: "samnth",
         content: "my goal is to get a job in a good company"
-    }
+    }   
 ];
 
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts: posts });
 });
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs")
+})
+
+app.post("/posts",(req,res)=>{
+let {username,content} = req.body;
+posts.push({username,content});
+res.redirect("/posts")
+
+}); 
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    // Render the page and send the final response
+    res.render("show.ejs", { post: post }); 
+});
+
+
 
 app.listen(port, () => {
     console.log("Listening to port:", port);
