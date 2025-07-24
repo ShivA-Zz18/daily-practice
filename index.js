0015f6454d17fbcf4332f41bda9c729d8232b337
Shiva kumar <shiva959154@gmail.com>
2025-07-24 22:09:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,89 +1,34 @@
-const { log } = require("console");
-const express = require("express");
-const app = express();
-const port = 8080;
-const path = require("path");
-const { v4: uuidv4 } = require('uuid');
-const methodOverride = require('method-override');
+const { faker } = require('@faker-js/faker');
+import mysql from 'mysql2/promise';
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-
-let posts = [
-    {
-        id: uuidv4(),
-        username: "shiva",
-        content: "i love watching movie"
-    },
-    {
-        id: uuidv4(),
-        username: "aman",
-        content: "i love watching coding"
-    },
-    {
-        id: uuidv4(),
-        username: "samnth",
-        content: "my goal is to get a job in a good company"
-    }
-];
-
-app.get("/posts", (req, res) => {
-    res.render("index.ejs", { posts: posts });
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'DELTA',
+  password:'SHIVA'
 });
 
-app.get("/posts/new", (req, res) => {
-    res.render("new.ejs");
-});
+let  getRandomUser =()=> {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(), 
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
 
-app.post("/posts", (req, res) => {
-    let { username, content } = req.body;
-    let id = uuidv4();
-    posts.push({ id, username, content });
-    res.redirect("/posts");
-});
+try{
+    connection.query("SHOW TABLES",(ERR,RES)=>{
+    if (ERR) throw ERR;
+    console.log(RES);
+    
+})
 
-app.get("/posts/:id", (req, res) => {
-    let { id } = req.params;
-    let post = posts.find((p) => id === p.id);
-    if (!post) {
-        return res.status(404).send("Post not found");
-    }
-    res.render("show.ejs", { post: post });
-});
+}catch(ERR){
+    console.log(ERR);
+    
 
-app.get("/posts/:id/edit", (req, res) => {
-    let { id } = req.params;
-    let post = posts.find((p) => id === p.id);
-    if (!post) {
-        return res.status(404).send("Post not found");
-    }
-    res.render("edit.ejs", { post });
-});
-
-app.patch("/posts/:id", (req, res) => {
-    let { id } = req.params;
-    let newContent = req.body.content;
-    let post = posts.find((p) => id === p.id);
-
-    if (post) {
-        post.content = newContent;
-        res.redirect(`/posts/${id}`);
-    } else {
-        res.status(404).send("Post not found");
-    }
-});
-
-app.delete("/posts/:id", (req, res) => {
-    let { id } = req.params;
-    posts = posts.filter((p) => id !== p.id);
-    res.redirect("/posts");
-});
+}
 
 
-app.listen(port, () => {
-    console.log("Listening to port:", port);
-});
+// console.log(getRandomUser());
