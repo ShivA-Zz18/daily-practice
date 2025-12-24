c6a8aaad7a30d1676fc001b55fbebed38571dfda
Shivakumar <shiva959154@gmail.com>
2025-12-24 22:27:11 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,405 +1,428 @@
-// console.log("Hello");
+// let a = 10;
+// let b = 30;
+
+// console.log(a+b);
+
+
+// let a= 10;
+// let b=20;
+
+
+// console.log("sun of 10 and 20 is :",(a+b));
+
+
+// let age = Number(prompt("Enter your age"));
+
+
+// console.log(age);
 
 
 
-// function Arry(){
-//     let Array=["Sman","rajath","Vikyath","Varun"];
-//     console.log(Array);
+// let a =10;
+// let b=20;
+
+// let c ;
+
+// c=a;
+// a=b;
+// b=c;
+
+
+// console.log(a);
+// console.log(b);
+
+
+// let a =10;
+// let b= 20;
+
+
+// a=a+b;//10+20=30
+
+// b=a-b;// 30-20=10
+
+// a=a-b; //30-10=20
+
+
+// console.log(a,b);
+
+
+
+// let a =10;
+// let b=20;
+
+// [a,b]=[b,a]
+
+
+// console.log(a,b);
+
+
+// let i = 11;
+
+// i=i++ ;
+
+// console.log(i);
+
+
+// let i =11;
+
+// i=++i;
+
+// console.log(i);
+
+
+// let a =11;
+// let b=22;
+// let c= a+b + a++ + b++ + ++a + ++b ;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// a=10;
+
+// // a=a+1;
+
+// a++;
+
+// console.log(a);
+
+
+
+
+//Day 2 conditional satements and Loops
+
+
+// let age = Number(prompt("Enter your Age :"));
+
+// if (isNaN(age)) {
+//     console.log("Wrong input");
+// }
+
+// else if (age>=18) {
+// console.log("You can Vote");
+// }
+
+
+// else{
+//     console.log("you can't vote ");
+
+// }
+
+
+//Discount Problem
+
+// let Amount = Number(prompt("Enter the Amount Hear : "))
+
+// if (Amount>0 && Amount<=5000) {
+//     console.log(Amount);
+
+// }
+
+// else if(Amount>=5001 && Amount<=7000){
+//     console.log(Math.floor(Amount-5*Amount/100));
+// }
+
+// else if(Amount>=7001 && Amount<=9000){
+//     console.log(Math.floor(Amount-10*Amount/100));
+// }
+
+// else if( Amount>9000){
+
+//     console.log(Math.floor(Amount-20*Amount/100))
+// }
+
+// else{
+//     console.log("Wrong input");
+
+// }
+
+
+// let Amount = Number(prompt("Enter the Amount Hear : "))
+// let Discount=0;
+
+// if (Amount>0 && Amount<=5000) {
+//    Discount=0;
+// }
+
+// else if(Amount>=5001 && Amount<=7000){
+//     Discount=5;
+// }
+
+// else if(Amount>=7001 && Amount<=9000){
+//     Discount=10;
+// }
+
+// else if( Amount>9000){
+//     Discount=20;
+
+// }
+
+// else{
+//     console.log("Wrong input");
+
+// }
+
+//  console.log(Math.floor(Amount-Discount*Amount/100))
+
+
+
+
+// let unit = Number(prompt("Enter electricity Bill   :"));
+// let Amount=0;
+
+// if (unit > 400) {
+// Amount=(unit- 400)*13
+//     unit=400;
+// }
+
+// if (unit<=400 && unit>200 ) {
+// Amount+=(unit-200)*8;
+//     unit=200;
+// }
+
+// if (unit<=200 && unit>100 ) {
+// Amount+=(unit-100)*6
+//     unit=100;
+// }
+
+
+// Amount+=(unit-100)*4
+
+
+// console.log(Amount);
+
+
+
+// let unit = Number(prompt("Enter electricity Bill :"));
+// let Amount = 0;
+
+// if (unit > 400) {
+//   Amount = (unit - 400) * 13;
+//   unit = 400;
+// }
+// if (unit <= 400 && unit > 200) {
+//   Amount += (unit - 200) * 8;
+//   unit = 200;
+// }
+// if (unit <= 200 && unit > 100) {
+//   Amount += (unit - 100) * 6;
+//   unit = 100;
+// }
+// if (unit <= 100) {
+//   Amount += unit * 4;
+// }
+
+// console.log(Amount);
+
+
+// let Amount = 2328;
+
+// if(Amount>=500){
+//    console.log("500 notes: "+Math.floor(Amount/500));
+//     Amount=Amount % 500;
+// }
+
+// if(Amount>=200){
+//    console.log("200 notes: "+Math.floor(Amount/200));
+//     Amount=Amount % 200;
+// }
+
+// if(Amount>=100){
+//    console.log("100 notes: "+Math.floor(Amount/100));
+//     Amount=Amount % 100;
+// }
+
+// if(Amount>=50){
+//    console.log("50 notes: "+Math.floor(Amount/50));
+//     Amount=Amount % 50;
+// }
+
+// if(Amount>=20){
+//    console.log("20 notes: "+Math.floor(Amount/20));
+//     Amount=Amount % 20;
+// }
+
+// if(Amount>=10){
+//    console.log("10 notes: "+Math.floor(Amount/10));
+//     Amount=Amount % 10;
+// }
+
+// if(Amount>=5){
+//    console.log("5 notes: "+Math.floor(Amount/5));
+//     Amount=Amount % 5;
+// }
+
+// if(Amount>=2){
+//    console.log("2 notes: "+Math.floor(Amount/2));
+//     Amount=Amount % 2;
+// }
+
+// if(Amount>=1){
+//    console.log("1 notes: "+Math.floor(Amount/1));
+//     Amount=Amount % 1;
+// }
+
+
+
+
+// let Number = Math.floor(0.1+ 0.2);
+
+// switch (Number) {
+//   case 0.3:
+//     console.log("Hello");
+//     break;
+
+//   case 0.5:
+//     console.log("heyy");
+//     break;
+
+//   default:console.log("namaste");
+  
     
 // }
 
-// Arry();
 
 
 
-// function poem(){
-// console.log("jony jony YEss papap");
-// console.log("Eating shugar no papap");
-// console.log("Up above the hands of high");
-// console.log("like dimend in the sky");
 
+// day3
+
+// loops
+
+
+// for(let i=1;i<=20;i++){
+//   console.log(i);
+  
+// }
+
+
+// for(let i=200;i>=100;i--){
+//   console.log(i);
+  
+// }
+
+
+// let n=Number(prompt("Enter the number"));
+
+// if(n<=0){
+// console.log("invalid input");
 
 // }
 
-// poem();
+// else if(n>0){
+// let sum =0;
 
+// for( let i=1;i<=n; i++){
 
-
-
-// function diseprint() {
-
-//     let dise=Math.floor(Math.random()*6)+1
-
-//     console.log(`Dise number is : ${dise}`);
-    
-    
+//   sum = sum + i
+  
+ 
+// }
+//  console.log(sum);
 // }
 
-// diseprint();
-
-
-// function stdInfo(name,age,gender){
-//     console.log(`student name is:${name} & age is ${age} gennder is ${gender}`);
-    
+// else{
+//   console.log("plese enter a number");
+  
 // }
 
-// stdInfo("rajath",18,"Male");
 
 
-// function printavg(a,b,c){
-//      avg =a+b+c/3;
-//      console.log(avg);
-     
+
+// let n=Number(prompt("Enter the number"));
+
+// if(n<=0){
+// console.log("invalid input");
+
 // }
 
-// printavg(10,20,30);
+// else if(n>0){
+
+ 
+// }
 
 
-// function table(n){
-   
-// for(let i=n;i<=n*10;i=i+n){
+// else{
+//   console.log("plese enter a number");
+  
+// }
+
+
+
+// let i =1;
+
+// while(i<=25){
 //     console.log(i);
-    
 // }
-
-// }
-
-// table(10);
-
-
-
-
-// let dise=Math.floor(Math.random()*6)+1;
-
-// console.log(`dise value is:  ${dise}`);
-
-
-
-// let car={
-// model:"jemini",
-// name:"ambasider",
-// color:"black"
-// };
-
-
-// console.log(car);
-
-
-
-// let Person={
-// name:"Amith",
-// age:23,
-// city:"rajasthan"
-// };
-
-// console.log(Person);
-
-
-
-
-
-// let arr = [8, 9, 10, 1, 2, 3, 4, 5, 6, 7];
-// let num = 5;
-// //elements larger than a number num
-// function getElements(arr, num) {
-// for (let i = 0; i < arr.length; i++) {
-// if (arr[i] > num) {
-// console.log(arr[i]);
-// }
-// }
-// }
-// getElements(arr, num);
-
-
-
-
-
-// let str = "abcdabcdefgggh";
-
-// // Function to get String with all unique elements
-// function getUnique(str) {
-//   function removeDuplicates(str) {
-//     let ans = ""; 
-//     for (let i = 0; i < str.length; i++) {
-//       let currChar = str[i];
-//       if (ans.indexOf(currChar) === -1) { 
-//         ans += currChar; 
-//       }
-//     }
-//     return ans; 
-//   }
-
-//   // Call the removeDuplicates function and return the result
-//   return removeDuplicates(str); 
-// }
-
-// // Call the getUnique function and print the result
-// let uniqueStr = getUnique(str); 
-// console.log(uniqueStr); // Output: "abcdcfg"
-
-
-
-// let country = ["Australia", "Germany", "United States of America"];
-//     function longestName(country) {
-//     let ansIdx = 0;
-//     for (let i = 0; i < country.length; i++) {
-//     let ansLen = country[ansIdx].length;
-//     let currLen = country[i].length;
-//     if (currLen > ansLen) {
-//     ansIdx = i;
-//     }
-//     }
-//     return country[ansIdx];
-//     }
-//     longestName(country);
-
-//     let longestCountry = longestName(country); 
-//     console.log(longestCountry); 
-
-
-
-// let str = "apnacollege";
-// function countVowels(str) {
-// let count = 0;
-// for (let i = 0; i < str.length; i++) {
-// if (
-// str.charAt(i) == "a" ||
-// str.charAt(i) == "e" ||
-// str.charAt(i) == "i" ||
-// str.charAt(i) == "o" ||
-// str.charAt(i) == "u"
-// ) {
-// count++;
-// }
-// }
-// return count;
-// }
-
-// let vowelCount = countVowels(str); 
-// console.log(vowelCount); 
-
 
 
 // console.log("hello");
 
-// const  sum=(a,b) =>{
-//     console.log(a+b);
+
+
+// for(let i=1;i<=6001;i++){
+//     console.log("hello");
+    
+// }
+
+
+// let ans=Number(prompt("ENter tha number")); 
+
+
+
+
+
+
+
+// process.stdout.write("hello")
+// process.stdout.write("world")
+
+// var prompt = require('prompt-sync')();
+
+// let n= Number(prompt("Enter the number"))
+
+// for(let i=1;i<=n;i++){
+//    for(let j=1;j<=i;j++){
+//     process.stdout.write("* ")
+//    }
+//    console.log();
+    
+// }
+
+
+// for(let i=1;i<=n;i++){
+//     for(let j=n;j>=i;j--){
+//         process.stdout.write("* ")
+//     }
+//     console.log();
     
 // }
 
 
 
-// console.log("Hello there!");
-// setTimeout(()=>{
-//     console.log("Apna college");4
-    
-// },3000);
 
+// let arr=[];
 
-// console.log("Welocome to ");
+// arr.push(100);
 
-
-// let arr=["raju",23,"moodabidre"];
-
-// let print=function(ele){
-//     console.log(ele);
-    
-// };
-
-// arr.forEach(print);
-
-
-// let arr=["shiva","karkal"];
-
-// arr.forEach((nam)=>{
-//     console.log(nam);
-    
-// });
- 
-
-// let arr=[{
-//     name:"bavandar",
-//     marks:93
-// },
-
-// {name:"ramulu",
-//     marks:99},
-
-// {name:"gintu",
-//  marks:92}];
-
-
-//  let gpa=arr.map((el) =>{
-// return el.marks/10;
-//  });
+// console.log(arr);
 
 
 
-// arr.forEach(function(st){
-//     console.log(st.marks );
-    
-// });
 
+//  let arr=[10,20,55,100,255];
 
-// let num=[1,2,3,4,5,6,7,8,9,10];
+//  let max=arr[0];
 
-// let ans=num.filter((el)=>{
-//     return el<5;
-
-// });
-
-
-
-// let Arry=[1,2,3,4,10];
-
-// let max=Arry.reduce((max,el)=>{
-//     if (max < el) {
-//         return el;
+//  for(let i=1;i< arr.length;i++){
+//     if(max<arr[i]){
+       
+//         max=arr[i];
         
-//     }else{
-//         return max;
 //     }
-// });
+     
+//  }
 
 
 // console.log(max);
-
-
-
-// let arr=[10,20,30,40];
-
-// let ans= arr.every((el)=> el % 10==0);
-
-// console.log(ans);
-
-
-// let arr=[10,3,2,5,1];
-
-// let min=arr.reduce((min,el)=>{
-//     if (min>el) {
-//         return el;
-        
-//     }
-//     else{
-//         return min;
-//     }
-// })
-
-// console.log(min);
-
-
-// let arr =[1,2,3,4,5,6,7,9,];
-
-// ans=Math.max(...arr);
-
-// console.log(ans);
-
-
-// console.log(...arr);
-
-
- 
-// // Define the list of fruits with their point values
-// let fruits = [
-//     { name: "Apple", points: 10 },
-//     { name: "Banana", points: 20 },
-//     { name: "Cherry", points: 30 },
-//     { name: "Date", points: 40 },
-//     { name: "Elderberry", points: 50 }
-//   ];
-  
-//   // Initialize the player's score and time limit
-//   let score = 0;
-//   let timeLimit = 60; // seconds
-  
-//   // Function to collect a fruit
-//   function collectFruit(fruit) {
-//     score += fruit.points;
-//     console.log(`Collected ${fruit.name}! Score: ${score}`);
-//   }
-  
-//   // Function to start the game
-//   function startGame() {
-//     console.log("Game started! Collect as many fruits as possible within 60 seconds.");
-//     fruits.forEach(fruit => {
-//       // Simulate the player collecting a fruit
-//       setTimeout(() => {
-//         collectFruit(fruit);
-//       }, Math.random() * timeLimit * 1000); // random delay between 0 and timeLimit seconds
-//     });
-//     setTimeout(() => {
-//       console.log("Time's up! Final score: " + score);
-//     }, timeLimit * 1000); // end the game after timeLimit seconds
-//   }
-
-
-// startGame();
-
-
-// let nums=[1,2,3,4,5];
-
-// const square = nums.map((num)=> num * num);
-
-// console.log(square);
-
-// let sum = square.reduce((acc, cur )=> acc + cur ,0);
-
-// let avg = sum / nums .length;
-
-// console.log(avg);
-
-
-
-// let numbers=[2,3,4,6,8,-2,-4];
-
-// console.log(numbers.map((Number)=> Number+5));
-
-
-
-// let strings =["adam","bob","catlyn","donald","eve"];
-
-// console.log(strings.map((strings)=> strings.toUpperCase()));
-
-
-
-// const doubleAndReturnArgs= (arr, ...args)=>[...arr,...args.map((v)=> v * 2),];
-
-// doubleAndReturnArgs([1,2,3,],4,4);
-// doubleAndReturnArgs([2],10,4);
-
-
-
-
-
-// const mergeObj=(obj1, obj2)=>({...obj1,...obj2});
-
-// mergeObj ({a: 1,  b:2},{c:3,d:4});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
- 
-
