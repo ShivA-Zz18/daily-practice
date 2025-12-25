41a9ed3aee5f83f72a77aa9d2ff9d494d50fe308
Shivakumar <shiva959154@gmail.com>
2025-12-25 08:01:39 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -426,3 +426,38 @@
 
 
 // console.log(max);
+
+
+
+// let arr=[1,1,0,1,0,1,0,1,0,0,1];
+
+// let i=0,j=0;
+
+// while(i<arr.length){
+//     if(arr[i]==0){
+//         let temp=arr[i]
+//         arr[i]=arr[j]
+//         arr[j]=temp;
+//         j++
+//     }
+//     i++
+// }
+
+// console.log(arr);
+
+
+// let arr=[1,2,3,4,5,6];
+// let copy= arr[0];
+
+// for(let i=0;i<arr.length-1;i++){
+//     arr[i]=arr[i]+1;
+// }
+
+// arr[arr.length-1]=copy;
+// console.log(arr);
+
+
+
+
+
+
